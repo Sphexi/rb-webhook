eefc9657d216085a82d39eca28b59513e5b6ba5f
Sphexi <git@sphexi.com>
2021-07-05 15:23:42 -0700

    Update app.py
    Trying to figure out a weird linode vps thing, just outputting some data to make sure it's correct

--- app.py
+++ app.py
@@ -52,6 +52,12 @@ Request Body: {}
 			""".format(datetime.date.today(),datetime.datetime.now().time(),request.remote_addr,request.method,json.dumps(dict(request.headers)),request.content_type,json.dumps(request.form.to_dict()))
 			print("form data submitted")
 
+
+		print(os.environ.get('SMTP-ADDR'))
+		print(os.environ.get('SMTP-USER'))
+		print(os.environ.get('SMTP-PASS'))
+		print(os.environ.get('ALERT-EMAIL'))
+		
 		message = "Subject: {}\n\n{}".format("Webhook test",emailBody)
 		server = smtplib.SMTP_SSL(os.environ.get('SMTP-ADDR'), 465)
 		server.starttls()
