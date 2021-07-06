1f9626d9a1b37e311e353c3c70100775943ce450
Sphexi <git@sphexi.com>
2021-07-05 18:29:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys, smtplib, json, html, email, datetime, os
 from flask import Flask, request, abort
+from email.message import EmailMessage
 
 app = Flask(__name__)
 
@@ -28,7 +29,7 @@ def webhook():
 
 
 				Request Body: {}
-				""".format(datetime.date.today(),datetime.datetime.now().time(),request.remote_addr,request.method,json.dumps(request.headers.to_dict()),request.content-type,json.dumps(request.json))
+				""".format(datetime.date.today(),datetime.datetime.now().time(),request.remote_addr,request.method,json.dumps(dict(request.headers)),request.content-type,json.dumps(request.json))
 			print(request.json)
 			print(json.dumps(request.json))
 		elif request.form is not None:
@@ -54,14 +55,17 @@ def webhook():
 		else:
 			abort(400)
 
-		message = "Subject: {}\n\n{}".format("Webhook test",emailBody)
-		sendTo = os.environ.get('ALERT-EMAIL')
-		sendFrom = os.environ.get('FROM-EMAIL')
+		message = EmailMessage()
+		message['Subject'] = 'Recurring Billing Webhook'
+		message['From'] = os.environ.get('FROM-EMAIL')
+		message['To'] = os.environ.get('ALERT-EMAIL')
+		message.set_content = emailBody
+		
 		server = smtplib.SMTP_SSL(os.environ.get('SMTP-ADDR'), 465)
 
 		try:
 			server.login(os.environ.get('SMTP-USER'), os.environ.get('SMTP-PASS'))
-			server.sendmail(sendFrom,sendTo,message)
+			server.send_message(message)
 		finally:
 			server.quit()
 		return '', 200
